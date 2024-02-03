8ba4a95fb9dbaa095a52f613cb0b27c59c26c486
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-03 13:36:47 +0300

    commoent create in navbar

--- src/layout/dashboard/DashboardLayout.vue
+++ src/layout/dashboard/DashboardLayout.vue
@@ -3,13 +3,13 @@
     <side-bar>
       <template slot="links">
         <sidebar-link to="/dashboard" :name="$t('sidebar.dashboard')" icon="tim-icons icon-chart-pie-36"/>
-        <sidebar-link to="/icons" :name="$t('sidebar.icons')" icon="tim-icons icon-atom"/>
+        <!-- <sidebar-link to="/icons" :name="$t('sidebar.icons')" icon="tim-icons icon-atom"/>
         <sidebar-link to="/maps" :name="$t('sidebar.maps')" icon="tim-icons icon-pin"/>
         <sidebar-link to="/notifications" :name="$t('sidebar.notifications')" icon="tim-icons icon-bell-55"/>
         <sidebar-link to="/profile" :name="$t('sidebar.userProfile')" icon="tim-icons icon-single-02"/>
         <sidebar-link to="/table-list" :name="$t('sidebar.tableList')" icon="tim-icons icon-puzzle-10"/>
         <sidebar-link to="/typography" :name="$t('sidebar.typography')" icon="tim-icons icon-align-center"/>
-         <sidebar-link to="/agents" :name="$t('agents')" icon="tim-icons icon-puzzle-10"/>
+         <sidebar-link to="/agents" :name="$t('agents')" icon="tim-icons icon-puzzle-10"/> -->
         <sidebar-link to="/accomodations" :name="$t('accomodations')" icon="tim-icons icon-puzzle-10"/>
         <sidebar-link to="/contracts" :name="$t('contracts')" icon="tim-icons icon-puzzle-10"/>
       </template>
