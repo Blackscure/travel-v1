8a993eace113d30486ec24aa7fe33b83204cb947
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-03 15:19:31 +0300

    form added

--- src/pages/CreateAgent.vue
+++ src/pages/CreateAgent.vue
@@ -2,20 +2,29 @@
   <card>
     <form @submit.prevent>
       <base-input
+        label="First  Name"
+        type="first_name"
+        placeholder="First Name"
+      ></base-input>
+
+      <base-input
+        label="Last Name"
+        type="email"
+        placeholder="Last Name"
+      ></base-input>
+
+      <base-input
         label="Email address"
         type="email"
         placeholder="Enter email"
-      >
-        <small slot="helperText" id="emailHelp" class="form-text text-muted">
-          We'll never share your email with anyone else.
-        </small>
-      </base-input>
-      <base-input
-        label="Password"
-        type="password"
-        placeholder="Password"
       ></base-input>
-      <base-checkbox>Option one is this</base-checkbox>
+
+      <base-input
+        label="passport Number"
+        type="passport_number"
+        placeholder="passport_number"
+      ></base-input>
+
       <base-button native-type="submit" type="primary">Submit</base-button>
     </form>
   </card>
