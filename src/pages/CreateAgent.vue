6cb9d87fb5672611ef8a5b4682cc9828aaf9ab6e
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-03 15:10:24 +0300

    form created

---
+++ src/pages/CreateAgent.vue
@@ -0,0 +1,22 @@
+<template>
+  <card>
+    <form @submit.prevent>
+      <base-input
+        label="Email address"
+        type="email"
+        placeholder="Enter email"
+      >
+        <small slot="helperText" id="emailHelp" class="form-text text-muted">
+          We'll never share your email with anyone else.
+        </small>
+      </base-input>
+      <base-input
+        label="Password"
+        type="password"
+        placeholder="Password"
+      ></base-input>
+      <base-checkbox>Option one is this</base-checkbox>
+      <base-button native-type="submit" type="primary">Submit</base-button>
+    </form>
+  </card>
+</template>
